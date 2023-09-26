04952ad26d42ddc92def6b701fbdc9add89efdb2
David Dominguez <dominguezdavid396@gmail.com>
2023-09-25 20:27:06 -0600

    jaj

--- Recinto.java
+++ Recinto.java
@@ -1,12 +1,19 @@
+import java.util.List;
+
 public class Recinto {
     private int id;
     private Ubicacion ubicacion;
     private int capacidad;
+    private List<Evento> eventos;
 
     public Recinto(int id, Ubicacion ubicacion, int capacidad) {
         this.id = id;
         this.ubicacion = ubicacion;
         this.capacidad = capacidad;
+    }
+
+    public void agregarEvento(Evento evento) {
+        eventos.add(evento);
     }
 
     // Otros métodos, como getters y setters.
